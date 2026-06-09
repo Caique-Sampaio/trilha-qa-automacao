062cdea075406f1f1f264cf15636a1e185c1422b
Caique-Sampaio <caique.sam93@gmail.com>
2026-06-08 22:29:33 -0300

    exercício objetos

---
+++ fundamentos/arrays.js
@@ -0,0 +1,13 @@
+// =============================================
+// Objetos — exercícios de prática
+// Trilha QA Automação
+// =============================================
+
+console.log("=== Início dos exercícios: Array ===\n");
+
+// ---------------------------------------------
+// Exercício 1 — Prática com arrays.
+// Objetivo: Exercitar a criação e manipulação de arrays.    
+// ---------------------------------------------
+console.log("EX1\n 1. Montar um array de objetos simulando cenários de teste.\n 2. Acessar o terceiro elemento e uma propriedade aninhada. \n 3. Filtrar só cenários ativo: true com filter. \n 4. Extrair todos os IDs com map. \n 5. Verificar se existe cenário com tag smoke usando some.\n");
+
--- fundamentos/objetos.js
+++ fundamentos/objetos.js
@@ -10,3 +10,48 @@ console.log("=== Início dos exercícios: Objetos ===\n");
 // Objetivo: Exercitar a criação e manipulação de objetos.    
 // ---------------------------------------------
 console.log("EX1\n 1. Montar um objeto de massa de teste com pelo menos três níveis de aninhamento.\n 2. Acessar propriedades com . e []. \n 3. Extrair campos com desestruturação (simples, aninhada e com renomeação). \n 4. Listar chaves de um subobjeto com Object.keys. \n 5. Serializar e parsear com JSON.stringify / JSON.parse.\n");
+
+const massaDeTeste = {
+    perfil: "admin",
+    usuario: {
+        nome: "Carla",
+        cpf: "05599037311",
+        endereco: {
+            rua: "X",
+            numero: "123",
+            referencia: "Y",
+            cidade: "Z",
+            estado: "A"
+        }
+    }
+};
+console.log(massaDeTeste.perfil, massaDeTeste.usuario["nome"]);
+
+const { perfil, usuario } = massaDeTeste;
+console.log(perfil, usuario);
+
+const {
+    usuario: {
+        nome,
+        endereco: { rua },
+    },
+} = massaDeTeste;
+console.log(nome, rua);
+
+const {
+    usuario: {
+        nome: nomeDoUsuario,
+        endereco: { rua: ruaDoUsuario },
+    },
+} = massaDeTeste;
+console.log(nomeDoUsuario, ruaDoUsuario);
+
+console.log(Object.keys(massaDeTeste));
+console.log(Object.keys(massaDeTeste.usuario));
+console.log(Object.keys(massaDeTeste.usuario.endereco));
+
+const jsonTexto = JSON.stringify(massaDeTeste);
+console.log(jsonTexto);
+
+const objetoTexto = JSON.parse(jsonTexto);
+console.log(objetoTexto);
